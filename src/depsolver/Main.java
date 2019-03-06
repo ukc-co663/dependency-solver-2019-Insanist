f616cc001f0e145622e0fd7d24904bfbe0013b85
kk410 <kk410@kent.ac.uk>
2019-03-06 14:58:26 +0000

    First testable version error corrected

--- src/depsolver/Main.java
+++ src/depsolver/Main.java
@@ -395,9 +395,12 @@ static List<String> constraintsMa = new ArrayList<>();
 
             String[] leSplit = q.split("<",2);
             boolean flag = false;
+            
             for (Package pLe : installed) {
               if (pLe.getName() == leSplit[0] && lex(pLe.getVersion(), leSplit[1]) < 0  ) {
                 flag = true;
+                System.out.println(q);
+                System.out.println(leSplit[0] + " " + leSplit[1 + " " + pLe.getName() +  " " + pLe.getVersion() ]);
               }
             }
             if (!flag) {
