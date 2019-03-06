c041e87ccbb45a366a5624181075c030123daf76
kk410 <kk410@kent.ac.uk>
2019-03-06 01:23:20 +0000

    Major overhaul of impl. based on whiteboard algo

--- src/depsolver/Main.java
+++ src/depsolver/Main.java
@@ -46,51 +46,308 @@ public class Main {
     Stack<Package> dfsStack = new Stack<>();
     List<String> dfsVisited = new ArrayList<>();
 
-    // Store positive and negative constraints to check against when installing
-    for (String i : constraints) {
-      if (i.charAt(0) == '+') {
-        String[] posTemp = i.split("=",2);
-        posConst.put(posTemp[0], posTemp[1]);
-      } else if (i.charAt(1) == '-') {
-        String[] negTemp = i.split("=",2);
-        negConst.put(negTemp[0], negTemp[1]);
+    // Store positive and negative constraints to check against when installing/uninstalling
+    if (constraints.size() != 0) {
+
+      for (String i : constraints) {
+        if (i.charAt(0) == '+') {
+          String[] posTemp = i.split("=",2);
+          posConst.put(posTemp[0], posTemp[1]);
+        } else if (i.charAt(1) == '-') {
+          String[] negTemp = i.split("=",2);
+          negConst.put(negTemp[0], negTemp[1]);
+        }
       }
+
     }
+
+    
     
 
-    HashMap<String, String> installed = new HashMap<>();
+    List<Package> installedPacks = new ArrayList<>();
     // Add packages from initial to installed HashMap
     for (String init : initial) {
       String[] temp = init.split("=",2);
-      installed.put(temp[0], temp[1]);
-    }
-
-    String commands = "[";
-
-    // 1. Start with constraint package
-    // 2. DFS traverse from start package
-    //  2a. Cycle through deps in alpha order
 
 
-    System.out.println(posConst.size());
-    boolean isFirst = true;
-    for (Package p : repo) {
-      // Attempt at seen 9
-      // If package isnt a negative constraint - install it
-      if (!negConst.containsKey(p.getName())) {
-        installed.put(p.getName(),p.getVersion());
-        if (isFirst) {
-          commands += p.getName() + "=" + p.getVersion();
-          isFirst = false;
-        } else {
-          commands += ",\"" + p.getName() + "=" + p.getVersion() + "\"";
+      for (Package p : repo) {
+        if (p.getName() == temp[0] && p.getVersion() == temp[1]) {
+          installedPacks.add(p);
         }
       }
     }
 
+    String commands = "[";
+
+
+
     commands += "]";
     
     System.out.println(commands);
+  }
+
+  static boolean isFinal(List<Package> installed, HashMap<String, String> positives, HashMap<String, String> negatives) {
+    
+    //TODO: Complete isFinal method
+
+    return null;
+
+  }
+
+  public static List<Package> search(List<Package> starting) {
+
+    List<Package> seen = new ArrayList<>();
+
+    // TODO: Complete search method as per Piazza
+
+    return null;
+  }
+
+  /*
+    isValid takes a package list and checks all dependencies and conflicts are met.
+  */
+  public static boolean isValid(List<Package> installed) {
+    //Dependencies Start
+    for (Package p1 : installed) {
+      List<List<String>> deps = new ArrayList<List<String>>();
+      if (p1.getDepends() != null) {
+        deps = p1.getDepends();
+      }
+      for (List<String> clause : deps) {
+        for (String q : clause) {
+          if (q.contains(">") && q.contains("=")) {
+
+            String[] grEqSplit = q.split(">=",2);
+            boolean flag = false;
+            for (Package pGrEq : installed) {
+              if (pGrEq.getName() == grEqSplit[0] && lex(pGrEq.getVersion(), grEqSplit[1]) >= 0  ) {
+                flag = true;
+              }
+            }
+            if (!flag) {
+              return false;
+            }
+
+            // Greater than or Equal To
+            // 1. Split into Package name and version
+            // 2. Loop through all installed packages
+            // 3. Find version name and check for correct version with compareTo()
+            // 4. If found set local oolean to true and continue looping
+            // 5. If not found immediately return FALSE overall
+
+          } else if (q.contains("<") && q.contains("=")) {
+            // Less Than or Equal To
+            String[] leEqSplit = q.split("<=",2);
+            boolean flag = false;
+            for (Package pLeEq : installed) {
+              if (pLeEq.getName() == leEqSplit[0] && lex(pLeEq.getVersion(), leEqSplit[1]) <= 0  ) {
+                flag = true;
+              }
+            }
+            if (!flag) {
+              return false;
+            }
+          } else if (q.contains(">")) {
+            // Greater Than
+            String[] grSplit = q.split("<=",2);
+            boolean flag = false;
+            for (Package pGr : installed) {
+              if (pGr.getName() == grSplit[0] && lex(pGr.getVersion(), grSplit[1]) > 0  ) {
+                flag = true;
+              }
+            }
+            if (!flag) {
+              return false;
+            }
+
+          } else if (q.contains("<")) {
+            // Less Than
+
+            String[] leSplit = q.split("<=",2);
+            boolean flag = false;
+            for (Package pLe : installed) {
+              if (pLe.getName() == leSplit[0] && lex(pLe.getVersion(), leSplit[1]) < 0  ) {
+                flag = true;
+              }
+            }
+            if (!flag) {
+              return false;
+            }
+          } else if (q.contains("=")) {
+            // Equals
+
+            String[] eqSplit = q.split("<=",2);
+            boolean flag = false;
+            for (Package pEq : installed) {
+              if (pEq.getName() == eqSplit[0] && lex(pEq.getVersion(), eqSplit[1]) == 0  ) {
+                flag = true;
+              }
+            }
+            if (!flag) {
+              return false;
+            }
+          } else {
+            // Any Version
+            for (Package any : installed) {
+              boolean flag = false;
+              if (any.getName() == q){
+                flag = true;
+              }
+            }
+
+            if (!flag) {
+              return false;
+            }
+          }
+        }
+      }
+    }
+    // Dependencies end
+
+    // Conflicts Start
+
+    for (Package p2 : installed) {
+
+      List<List<String>> confs = new ArrayList<List<String>>();
+      if (p2.getDepends() != null) {
+        confs = p2.getDepends();
+      }
+
+      for (List<String> clause : confs) {
+        for (String q : clause) {
+          if (q.contains(">") && q.contains("=")) {
+
+            String[] grEqSplit = q.split(">=",2);
+            boolean flag = false;
+            for (Package pGrEq : installed) {
+              if (pGrEq.getName() == grEqSplit[0] && lex(pGrEq.getVersion(), grEqSplit[1]) >= 0  ) {
+                flag = true;
+              }
+            }
+            if (flag) {
+              return false;
+            }
+
+            // Greater than or Equal To
+            // 1. Split into Package name and version
+            // 2. Loop through all installed packages
+            // 3. Find version name and check for correct version with compareTo()
+            // 4. If found set local oolean to true and continue looping
+            // 5. If not found immediately return FALSE overall
+
+          } else if (q.contains("<") && q.contains("=")) {
+            // Less Than or Equal To
+            String[] leEqSplit = q.split("<=",2);
+            boolean flag = false;
+            for (Package pLeEq : installed) {
+              if (pLeEq.getName() == leEqSplit[0] && lex(pLeEq.getVersion(), leEqSplit[1]) <= 0  ) {
+                flag = true;
+              }
+            }
+            if (flag) {
+              return false;
+            }
+          } else if (q.contains(">")) {
+            // Greater Than
+            String[] grSplit = q.split("<=",2);
+            boolean flag = false;
+            for (Package pGr : installed) {
+              if (pGr.getName() == grSplit[0] && lex(pGr.getVersion(), grSplit[1]) > 0  ) {
+                flag = true;
+              }
+            }
+            if (flag) {
+              return false;
+            }
+
+          } else if (q.contains("<")) {
+            // Less Than
+
+            String[] leSplit = q.split("<=",2);
+            boolean flag = false;
+            for (Package pLe : installed) {
+              if (pLe.getName() == leSplit[0] && lex(pLe.getVersion(), leSplit[1]) < 0  ) {
+                flag = true;
+              }
+            }
+            if (flag) {
+              return false;
+            }
+          } else if (q.contains("=")) {
+            // Equals
+
+            String[] eqSplit = q.split("<=",2);
+            boolean flag = false;
+            for (Package pEq : installed) {
+              if (pEq.getName() == eqSplit[0] && lex(pEq.getVersion(), eqSplit[1]) == 0  ) {
+                flag = true;
+              }
+            }
+            if (flag) {
+              return false;
+            }
+          } else {
+            // Any Version
+            for (Package any : installed) {
+              boolean flag = false;
+              if (any.getName() == q){
+                flag = true;
+              }
+            }
+
+            if (flag) {
+              return false;
+            }
+          }
+        }
+      }
+    }
+
+    // Conflicts End
+
+    return true;
+  }
+
+  /*
+    Lex calculates lexicographic comparison and returns the following
+    0 if equal
+    >0 if v1 is greater
+    <0 if v2 is greater
+  */
+  public static int lex(String v1, String v2) {
+    String[] x = v1.split(".");
+    String[] y = v2.split(".");
+
+    if (x.length > y.length) {
+      for (int i = 0; i < y.length; i ++) {
+        if (Integer.parseInt(x[i]) > Integer.parseInt(y[i])) {
+          return 1;
+        } else if (Integer.parseInt(x[i]) < Integer.parseInt(y[i])) {
+          return -1;
+        }
+      }
+      return 1;
+    } else if (x.length < y.length) {
+      for (int i = 0; i < x.length; i ++) {
+        if (Integer.parseInt(x[i]) > Integer.parseInt(y[i])) {
+          return 1;
+        } else if (Integer.parseInt(x[i]) < Integer.parseInt(y[i])) {
+          return -1;
+        }
+      }
+      return -1;
+      
+    } else {
+      for (int i = 0; i < y.length; i ++) {
+        if (Integer.parseInt(x[i]) > Integer.parseInt(y[i])) {
+          return 1;
+        } else if (Integer.parseInt(x[i]) < Integer.parseInt(y[i])) {
+          return -1;
+        }
+      }
+    }
+    return 0;    
+    
   }
 
   static String readFile(String filename) throws IOException {
